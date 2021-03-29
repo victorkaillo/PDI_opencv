0c797b3d1d22acf76df01a013720e1700a228170
Victor Kaillo <victorkaillo@ufrn.edu.br>
2021-03-29 20:54:16 -0300

    Create labeling_off_extremidas.cpp

---
+++ labeling_off_extremidas.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char** argv){
+  cv::Mat image, realce;
+  int width, height;
+  int nobjects;
+  
+  cv::Point p;
+  image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+  
+  if(!image.data){
+    std::cout << "imagem nao carregou corretamente\n";
+    return(-1);
+  }
+
+  width=image.cols;
+  height=image.rows;
+  std::cout << width << "x" << height << std::endl;
+
+  p.x=0;
+  p.y=0;
+
+  for(int i=0; i<height; i++){
+    if(image.at<uchar>(0, i) == 255){ 
+        floodFill(image,Point(i, 0), 0); /*exclui bolhas da primeira linha */
+    }
+    if(image.at<uchar>(width-1, i) == 255){ 
+        floodFill(image,Point(i, width-1), 0); /*exclui bolhas da ultima linha*/
+    }
+  } 
+
+  for(int i=0; i<width; i++){
+    if(image.at<uchar>(i,0) == 255){ 
+        floodFill(image,Point(0, i), 0); /*exuclui bolhas da primeira coluna*/
+    }
+    if(image.at<uchar>(i,height-1) == 255){ 
+        floodFill(image,Point(height-1, i), 0); /*exclui bolhas da ultima coluna*/
+    }
+  }
+
+  // busca objetos presentes
+  nobjects=0;
+  for(int i=0; i<height; i++){
+    for(int j=0; j<width; j++){
+      if(image.at<uchar>(i,j) == 255){
+        // achou um objeto
+        nobjects++;
+        p.x=j;
+        p.y=i;
+  		// preenche o objeto com 0 (asssim perdemos a saida com identificacao mas conseguimos contar normalmente o numero de bolhas mesmo com numero maior que 255)
+		  cv::floodFill(image,p,0);
+      }
+    }
+  }
+  std::cout << "a figura tem " << nobjects << " bolhas\n";
+  cv::equalizeHist(image, realce);
+  cv::imshow("image", image);
+  cv::imshow("realce", realce);
+  cv::imwrite("labeling_extremidade.png", image);
+  cv::imwrite("labeling_extremidade_realce.png", realce);
+  cv::waitKey();
+  return 0;
+}
