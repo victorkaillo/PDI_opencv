a75da35b9926024782d349e7dc6a0cb75d38788b
Victor Kaillo <victorkaillo@ufrn.edu.br>
2021-03-30 16:04:28 -0300

    labeling_sem extremidades

--- labeling_off_extremidas.cpp
+++ labeling_off_extremidas.cpp
@@ -6,7 +6,7 @@ using namespace cv;
 int main(int argc, char** argv){
   cv::Mat image, realce;
   int width, height;
-  int nobjects;
+  int nobjects, noburacos;
   
   cv::Point p;
   image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
@@ -50,17 +50,69 @@ int main(int argc, char** argv){
         nobjects++;
         p.x=j;
         p.y=i;
-  		// preenche o objeto com 0 (asssim perdemos a saida com identificacao mas conseguimos contar normalmente o numero de bolhas mesmo com numero maior que 255)
-		  cv::floodFill(image,p,0);
+  		/* preenche o objeto com 0 (asssim perdemos a saida com identificacao mas conseguimos contar normalmente o numero de bolhas mesmo com numero maior que 255)*/
+		  cv::floodFill(image,p,nobjects);
       }
     }
   }
+    //cv::imshow("image0", image);
+
+  /*fundo branco*/
+
+  floodFill(image,Point(0,0),255);
+
+
+  /**/
+  noburacos = 0;
+  for(int i=0; i<height; i++){
+    for(int j=0; j<width; j++){
+      if(image.at<uchar>(i,j) == 0 && (int)image.at<uchar>(i,j-1)>noburacos){
+        /*achou buraco*/
+        noburacos++;
+        p.x=j;
+        p.y=i;
+        floodFill(image,p,255);
+      }
+    }
+  }
+
+  floodFill(image,Point(0,0),0);
+
+  for(int i=0; i<height; i++){
+    for(int j=0; j<width; j++){
+      if(image.at<uchar>(i,j) == 255){
+        // achou um objeto
+        //nobjects++;
+        p.x=j;
+        p.y=i;
+      /* preenche o objeto com 0 (asssim perdemos a saida com identificacao mas conseguimos contar normalmente o numero de bolhas mesmo com numero maior que 255)*/
+      cv::floodFill(image,p,150);
+      }
+    }
+  }
+    floodFill(image,Point(0,0),255);
+
+  for(int i=0; i<height; i++){
+    for(int j=0; j<width; j++){
+      if(image.at<uchar>(i,j) == 150){
+        // achou um objeto
+        //nobjects++;
+        p.x=j;
+        p.y=i;
+      /* preenche o objeto com 0 (asssim perdemos a saida com identificacao mas conseguimos contar normalmente o numero de bolhas mesmo com numero maior que 255)*/
+      cv::floodFill(image,p,0);
+      }
+    }
+  }
+
+
   std::cout << "a figura tem " << nobjects << " bolhas\n";
+  std::cout << "a figura tem ao menos " << noburacos << " bolhas com buracos {nao consideramos eventuais buracos nas extremidades}\n";
   cv::equalizeHist(image, realce);
   cv::imshow("image", image);
   cv::imshow("realce", realce);
   cv::imwrite("labeling_extremidade.png", image);
-  cv::imwrite("labeling_extremidade_realce.png", realce);
+  //cv::imwrite("labeling_extremidade_realce.png", realce);
   cv::waitKey();
   return 0;
 }
