b60f47af62a35c8751790ef4dcf8e97ae878ec49
Victor Kaillo <victorkaillo@ufrn.edu.br>
2021-04-02 18:54:51 -0300

    kaillo png

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,7 @@ int main(int argc, char** argv){
   cv::Mat image;
   image = cv::imread(argv[1],cv::IMREAD_GRAYSCALE);
   cv::imshow("image", image);
+  cv::imwrite("kaillo.png", image);
   cv::waitKey();
   return 0;
 }
